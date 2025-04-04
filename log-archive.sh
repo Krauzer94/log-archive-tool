5d2226d33ed7491ea0ab169749b91b3494bdfe63
Krauzer94 <13894059+Krauzer94@users.noreply.github.com>
2025-04-04 00:46:47 -0300

    Create a function to check for existing dir

--- log-archive.sh
+++ log-archive.sh
@@ -4,10 +4,15 @@
 set -e
 
 # Check if log directory is provided
-if [ -z "$1" ]; then
-  echo ""
-  echo "Error: please provide a log directory"
-  echo "Usage: $0 <log-directory>"
-  echo ""
-  exit 1
-fi
+check_dir() {
+  if [ -z "$1" ]; then
+    echo ""
+    echo "Error: please provide a log directory"
+    echo "Usage: $0 <log-directory>"
+    echo ""
+    exit 1
+  fi
+}
+
+# Execute chain
+check_dir
